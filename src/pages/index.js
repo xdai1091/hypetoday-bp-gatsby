d73e98f0977cd04a52d32cbb32f20a3bd0657ca8
Xingchi Dai <xingchi.dai@XingchideMacBook-Pro.local>
2022-09-13 18:09:39 -0400

    scroll done

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ const IndexPage = () => {
       color: "white",
       height: "100%",
       width: "100%",
-      position: "fixed",
+      position: "relative",
       margin: '50px'
     }}>
 
@@ -22,19 +22,20 @@ const IndexPage = () => {
         Hypetoday为运动领队提供技术支持与服务 <br />一、完美的客户体验<br />二、实时的流量分析<br />三、机器学习赋能<br />四、提高品牌知名度
       </IntroBlock>
 
-      <div style={{marginTop:"50px"}}></div>
-      
+      <div style={{ marginTop: "50px" }}></div>
+
       <IntroBlock title="English Version" date="February,2022"><p> We are looking to be the Shopify of the fitness leader industry by providing: <br /> 1. Seamless Customer Experience <br /> 2. Real-time Traffic Analysis <br /> 3. Leverage Machine Learning <br /> 4. Boost Reputation and Brand Recognition</p>
       </IntroBlock>
 
 
       <div style={{
-        bottom:"0",
-        display:"flex",
-        width:"100%",
-        justifyContent:"space-around",
-        position:"absolute",
-        fontSize:"small"
+        bottom: "0",
+        display: "flex",
+        width: "100%",
+        justifyContent: "space-around",
+        position: "relative",
+        fontSize: "large",
+        marginTop:"200px"
       }}>
         <p>代星驰</p>
         <p>黔ICP备2021002523号</p>
